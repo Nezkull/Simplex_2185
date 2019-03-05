5587f0bd4563f14b20942abab21779bac5c09538
ngm9939 <ngm9939@IGM2000-04>
2019-03-04 20:03:32 -0500

    E08 Deliverable

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -53,27 +53,30 @@ void Application::Display(void)
 		m_pCamera->ResetCamera();
 		break;
 	case 2:
-		// m_pCamera->SetFOV(50.0f);
-		m_pCamera
-		m_pCamera->SetNearFar(vector2(-10.0f, 1.0f));
-		// do something else here
+		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false); // works
 		break;
 	case 3:
-		m_pCamera->SetPositionTargetAndUpward(vector3(20.0f, 0.0f, 0.0f), vector3(0.0f), vector3(-AXIS_Z)); // works
+		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(20.0f, 0.0f, 0.0f), vector3(0.0f), -AXIS_Z); // works
 		break;
 	case 4:
-		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -17.0f), vector3(0.0f), vector3(AXIS_Y)); // works
+		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -17.0f), vector3(0.0f), AXIS_Y); // works
 		break;
 	case 5:
-		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -17.0f), vector3(0.0f), vector3(AXIS_Y)); // works
+		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -17.0f), vector3(0.0f), AXIS_Y); // works
 		m_pCamera->SetNearFar(vector2(10.0f, -17.0f)); // works
 		break;
 	case 6:
-		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -15.0f), vector3(0.0f), vector3(AXIS_Y)); // works
+		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, -15.0f), vector3(0.0f), AXIS_Y); // works
 		m_pCamera->SetNearFar(vector2(0.01f, 10.0f)); // works
 		break;
 	case 7:
-		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, 10.0f), vector3(0.0f), vector3(-AXIS_Y)); // works
+		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0f, 0.0f, 10.0f), vector3(0.0f), -AXIS_Y); // works
 		break;
 	}
 
